2d1b16635c0073af302c4e624aed34d49ad42f84
Florian Scholz <scholzf98@gmail.com>
2020-07-03 15:03:08 +0200

    Initial Commit

---
+++ iWereWolves/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  iWereWolves
+//
+//  Created by Florian Scholz on 03.07.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iWereWolves/iWereWolvesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iWereWolvesApp.swift
+//  iWereWolves
+//
+//  Created by Florian Scholz on 03.07.20.
+//
+
+import SwiftUI
+
+@main
+struct iWereWolvesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
